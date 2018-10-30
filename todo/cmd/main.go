9e1e111adcc2fefec27f9a4d345fb4be5f97c788
Giang Le Ngoc <gianglengoc.hut@gmail.com>
2018-10-30 11:37:13 +0700

    init

---
+++ todo/cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import service "github.com/lengocgiang/todos/todo/cmd/service"
+
+func main() {
+	service.Run()
+}
---
+++ todo/pkg/db/db.go
@@ -0,0 +1,19 @@
+package db
+
+import (
+	mgo "gopkg.in/mgo.v2"
+)
+
+var mgoSession *mgo.Session
+var mgo_conn_str = "localhost:27017"
+
+func GetMongoSession() (*mgo.Session, error) {
+	if mgoSession == nil {
+		var err error
+		mgoSession, err = mgo.Dial(mgo_conn_str)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return mgoSession.Clone(), nil
+}
